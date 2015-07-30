fe204bd8ed254d96d4601e5891ba3d0a4d54a8f2
David Chan <david@dchanm.com>
2015-07-29 18:12:38 -0700

    Remove Android SDK dependency

--- com/sec/android/app/sbrowser/common/TabData.java
+++ com/sec/android/app/sbrowser/common/TabData.java
@@ -1,23 +1,22 @@
 package com.sec.android.app.sbrowser.common;
 
 import java.io.Serializable;
-import android.graphics.Bitmap.Config;
 
 public class TabData implements Serializable {
   private static final long serialVersionUID = -4581405572611034185L;
   private byte[] bitmapByteArray;
-  private Config mConfig;
+  private Object mConfig;
   private int height;
   private int width;
 
-  public TabData(byte[] arr, int width, int height, Config c) {
+  public TabData(byte[] arr, int width, int height, Object c) {
     this.bitmapByteArray = arr;
     this.width = width;
     this.height = height;
     this.mConfig = c;
   }
 
-  public Config getBitConfig() {
+  public Object getBitConfig() {
     return mConfig;
   }
 
